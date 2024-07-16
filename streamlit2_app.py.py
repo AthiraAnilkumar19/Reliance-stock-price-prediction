2efb28e391e9ce397a1b45aa5d69ff8ffd293eba
AthiraAnilkumar19 <athiraanilkumar19@gmail.com>
2024-07-16 13:44:47 +0530

    Update streamlit2_app.py.py

--- streamlit2_app.py.py
+++ streamlit2_app.py.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pickle
-from dat
+from datetime import datetime
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 import joblib
